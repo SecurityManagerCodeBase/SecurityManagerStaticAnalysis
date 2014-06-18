7c67633f9b3064e97d24c86b32096cdf947ed03b
Zack Coker <zfc@cs.cmu.edu>
2014-06-18 15:44:30 -0700

    Commiting a working basic FindBugs plugin

---
+++ src/DetectMultipleSecurityManagerSets.java
@@ -0,0 +1,39 @@
+import edu.umd.cs.findbugs.BugInstance;
+import edu.umd.cs.findbugs.BugReporter;
+import edu.umd.cs.findbugs.bcel.OpcodeStackDetector;
+
+
+public class DetectMultipleSecurityManagerSets extends OpcodeStackDetector {
+
+	private BugReporter bugReporter;
+	int smSetsSeen;
+	public DetectMultipleSecurityManagerSets(BugReporter br)
+	{
+		bugReporter = br;
+		smSetsSeen = 0;
+	}
+	@Override
+	public void sawOpcode(int opcodeSeen) {
+		boolean check = true;
+		String sigOperand = "";
+		try
+		{
+		  sigOperand = getSigConstantOperand().toString();
+		}
+		catch(IllegalStateException e)
+		{
+		  check = false;
+		}
+		if (check == true && opcodeSeen == INVOKESTATIC && getClassConstantOperand().equals("java/lang/System")
+                && getNameConstantOperand().equals("setSecurityManager") && sigOperand.equals("(Ljava/lang/SecurityManager;)V"))
+			//System.out.println("ConstantOperande: "+getSigConstantOperand().toString());
+			smSetsSeen = smSetsSeen + 1;
+		    if (smSetsSeen > 1)
+		    {
+		      bugReporter.reportBug(new BugInstance(this, "MULTIPLE_SECURITY_MANAGER_SET_BUG",HIGH_PRIORITY)
+		      .addClassAndMethod(this).addString("ConstantOperand: "+sigOperand).addSourceLine(this));
+		    }	
+	}
+	
+	
+}
